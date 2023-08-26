e5953fd0eac91baf0f907cf0457f3cd7f82d9336
DebAxom <debaxom@gmail.com>
2023-08-26 12:06:46 +0530

    Tweaking the code a little

--- bin/cli.js
+++ bin/cli.js
@@ -22,13 +22,13 @@ const commandsMap = {
     "compile": ()=> compileProject(NijorConfigFile,userArgs[1]),
     "serve": ()=> serveProject(),
     "ssg" : ()=> generateStaticSite(NijorConfigFile),
-    "-v": ()=> console.log('v3.6'),
+    "-v": ()=> console.log('v3.7'),
     "default": ()=> DefaultCommand()
 }
 
 function DefaultCommand(){
     console.print("Welcome to the Nijor CLI !",[0,195,255]);
-    console.print("version : 3.6",[0,195,255]);
+    console.print("version : 3.7",[0,195,255]);
 }
 
 try {
